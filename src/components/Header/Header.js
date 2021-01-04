ad4847da00c284d051ea68448fd1c8f4fb4a2843
Grant Wasil <dev@grantwasil.com>
2021-01-03 17:07:04 -0700

    added fonts and moved files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h3 className="header__title">NewsExplorer</h3>
+      <>
+        <a href="/">Home</a>
+      </>
+    </div>
+  );
+}
+
+export default Header;
