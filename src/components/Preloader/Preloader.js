cc6cf7fc7f516f78587296c2883f0c40848a56a6
Grant Wasil <dev@grantwasil.com>
2021-01-22 03:13:57 -0700

    fix: changed preloader into js

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Preloader.css'
+
+function Preloader() {
+  return (
+    <>
+      <i class="circle-preloader"></i>
+    </>
+  )
+}
+
+export default Preloader;
