643b99afc96887de4327e025d4fe600525549a4e
Grant Wasil <dev@grantwasil.com>
2021-03-02 18:28:22 -0700

    feat: implemented api

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Header from '../Header/Header';
-import SearchForm from '../SearchForm/SearchForm';
-import NewsCardList from '../NewsCardList/NewsCardList';
-import About from '../About/About';
-import Footer from '../Footer/Footer';
-import RegistrationPopup from '../RegistrationPopup/RegistrationPopup';
-import LoginPopup from '../LoginPopup/LoginPopup';
-import SavedNews from '../SavedNews/SavedNews';
-import Navigation from '../Navigation/Navigation';
-import Preloader from '../Preloader/Preloader';
-import './Main.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
+import NewsCardList from "../NewsCardList/NewsCardList";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import RegistrationPopup from "../RegistrationPopup/RegistrationPopup";
+import LoginPopup from "../LoginPopup/LoginPopup";
+import SavedNews from "../SavedNews/SavedNews";
+import Navigation from "../Navigation/Navigation";
+import NotFound from "../NotFound/NotFound";
+import Preloader from "../Preloader/Preloader";
+import api from '../../utils/MainApi';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
+import "./Main.css";
 
 function Main() {
-
   const [isLoginPopupOpen, setIsLoginPopupOpen] = React.useState(false);
-  const [isRegistrationPopupOpen, setIsRegistrationPopupOpen] = React.useState(false);
+  const [isRegistrationPopupOpen, setIsRegistrationPopupOpen] = React.useState(
+    false
+  );
   const [isHeaderMenuOpen, setIsHeaderMenuOpen] = React.useState(false);
+  const [keyword, setKeyword] = React.useState("");
+  const [results, setResults] = React.useState({});
+  const [isPreloaderShown, setIsPreloaderShown] = React.useState(false);
+  const [isNotFoundShown, setIsNotFoundShown] = React.useState(false);
+  const [user, setUser] = React.useState({});
+  const [userArticles, setUserArticles] = React.useState({});
 
+  React.useEffect(() => {
+    api.checkUserData()
+      .then((res) => {
+        if (res.data) {
+          setUser(res.data)
+          api.getArticles()
+            .then((res) => {
+              console.log(res.filter(a => a.owner ));
+            })
+        }
+      })
+  }, [])
 
   function onLogin() {
     setIsLoginPopupOpen(true);
@@ -30,7 +52,7 @@ function Main() {
     setIsRegistrationPopupOpen(true);
   }
 
-  function onMenu(){
+  function onMenu() {
     setIsHeaderMenuOpen(true);
   }
 
@@ -40,36 +62,118 @@ function Main() {
     setIsHeaderMenuOpen(false);
   }
 
+  function onSearch(input) {
+    setResults({});
+    setKeyword(input);
+    setIsNotFoundShown(false);
+    setIsPreloaderShown(true);
+    api.getSearchData(input)
+      .then(data => {
+        if (data.articles.length > 0) {
+          setResults(data)
+        } else {
+          setIsNotFoundShown(true);
+        }
+        setIsPreloaderShown(false);
+      })
+      .catch(e => console.log(e));
+  }
+
+  function onRegisterUser(email, password, username) {
+    api.registerUser(email, password, username)
+      .then((data) => {
+        if (data) {
+          setUser(data);
+          closeAllPopups();
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  function onLoginUser(email, password) {
+    api.loginUser(email, password)
+      .then((data) => {
+        if (data.token) {
+          api.checkUserData()
+            .then((res) => {
+              if (res.data) {
+                setUser(res.data);
+              }
+            })
+          closeAllPopups();
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  function onLogoutUser() {
+    localStorage.removeItem('jwt');
+    setUser({});
+    closeAllPopups();
+  }
+
+  function onSaveArticle(title, text, date, source, link, image) {
+    console.log(title);
+    api.saveArticle(keyword, title, text, date, source, link, image)
+      .then((data) => {
+        console.log(data);
+      })
+  }
+
   return (
-    <>
+    <CurrentUserContext.Provider value={user}>
       <Route exact path="/">
         <div className="main__search">
-          {isHeaderMenuOpen
-            ? <Navigation handleClose={closeAllPopups} handleLogin={onLogin} />
-            : <Header page="home" handleLogin={onLogin} handleLogout={closeAllPopups} handleMenu={onMenu} />
-          }
+          {isHeaderMenuOpen ? (
+            <Navigation handleClose={closeAllPopups} handleLogin={onLogin} />
+          ) : (
+            <Header
+              page="home"
+              handleLogin={onLogin}
+              handleLogout={onLogoutUser}
+              handleMenu={onMenu}
+            />
+          )}
 
-          <SearchForm />
+          <SearchForm handleSearch={onSearch}/>
         </div>
-        <Preloader />
-        <NewsCardList page="home" />
+        {isPreloaderShown ? <Preloader /> : <></>}
+        {isNotFoundShown ? <NotFound /> : <></>}
+        {results.articles ? <NewsCardList page="home" results={results} handleSaveClick={onSaveArticle} /> : <> </>}
         <About />
         <Footer />
       </Route>
       <Route path="/saved-news">
         <div className="main__user">
-          {isHeaderMenuOpen
-            ? <Navigation handleClose={closeAllPopups} handleLogin={onLogin} />
-            : <Header user="Test" page="news" handleLogin={onLogin} handleLogout={closeAllPopups} handleMenu={onMenu} />
-          }
+          {isHeaderMenuOpen ? (
+            <Navigation handleClose={closeAllPopups} handleLogin={onLogin} />
+          ) : (
+            <Header
+              user="Test"
+              page="news"
+              handleLogin={onLogin}
+              handleLogout={closeAllPopups}
+              handleMenu={onMenu}
+            />
+          )}
         </div>
         <SavedNews />
         <NewsCardList page="news" />
         <Footer />
       </Route>
-      <RegistrationPopup isOpen={isRegistrationPopupOpen} onClose={closeAllPopups} onLink={onLogin}/>
-      <LoginPopup isOpen={isLoginPopupOpen} onClose={closeAllPopups} onLink={onRegister} />
-    </>
+      <RegistrationPopup
+        isOpen={isRegistrationPopupOpen}
+        onClose={closeAllPopups}
+        onLink={onLogin}
+        handleRegisterUser={onRegisterUser}
+      />
+      <LoginPopup
+        isOpen={isLoginPopupOpen}
+        onClose={closeAllPopups}
+        onLink={onRegister}
+        handleLoginUser={onLoginUser}
+      />
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,11 +1,114 @@
 class Api {
-  constructor({ baseUrl, authentication }) {
-    this._baseUrl = baseUrl;
+  constructor({ newsApi, nxplrApi }) {
+    this._newsApi = newsApi;
+    this._nxplrApi = nxplrApi;
+  }
+
+  getSearchData(input) {
+    return fetch(`${this._newsApi}&q=${input}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  registerUser(email, password, name) {
+    return fetch(`${this._nxplrApi}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password,
+        email,
+        name,
+      }),
+    })
+      .then((res) => {
+        try {
+          if (res.status === 201) {
+            return res.json();
+          }
+        } catch (e) {
+          return e;
+        }
+      })
+      .then((res) => {
+        return res;
+      });
+  }
+
+  loginUser(email, password) {
+    return fetch(`${this._nxplrApi}/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("jwt", data.token);
+          return data;
+        }
+      });
+  }
+
+  checkUserData() {
+    return fetch(`${this._nxplrApi}/users/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    }).then((res) => res.json());
+  }
+
+  getArticles() {
+    return fetch(`${this._nxplrApi}/articles`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    }).then((res) => res.json());
+  }
+
+  saveArticle(keyword, title, text, date, source, link, image) {
+    return fetch(`${this._nxplrApi}/articles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        keyword,
+        title,
+        text,
+        date,
+        source,
+        link,
+        image,
+      }),
+    }).then((res) => res.json());
   }
 }
 
+const today = new Date().toISOString();
+const lastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString();
+
 const api = new Api({
-  baseUrl: "https://nomoreparties.co/news/v2/top-headlines?country=us&apiKey=29fdf2c3653342b698ddcf64e505ee71"
-})
+  newsApi: `https://nomoreparties.co/news/v2/everything?apiKey=29fdf2c3653342b698ddcf64e505ee71&pageSize=100&from=${lastWeek}&to=${today}`,
+  nxplrApi: `https://api.nxplr.com`,
+});
 
 export default api;
