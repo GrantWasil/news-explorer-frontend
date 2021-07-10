98f723c8b48e4c252b7c7e066ea90454306f540f
Grant Wasil <dev@grantwasil.com>
2021-07-10 02:51:44 -0600

    feat: updated footer with personal links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,17 +5,14 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
+      <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
       <div className="footer__links">
         <Link to="/" className="footer__links-item">Home</Link>
-        <a href="https://practicum.yandex.com" target="_blank" rel="noreferrer" className="footer__links-item">Practicum by Yandex</a>
+        <a href="https://grantwasil.com" target="_blank" rel="noreferrer" className="footer__links-item">Grant Wasil</a>
       </div>
       <div className="footer__logos">
         <a href="https://github.com/GrantWasil" target="_blank" rel="noreferrer">
           <div className="footer__links-logo footer__github" />
-        </a>
-        <a href="https://www.facebook.com/YPracticum/" target="_blank" rel="noreferrer">
-          <div className="footer__links-logo footer__facebook" />
         </a>
       </div>
     </footer>
